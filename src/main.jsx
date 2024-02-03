8610c612589740963807f14975319ea17cf83e3c
Iván Navas <ivanrng.work@gmail.com>
2024-02-03 13:42:21 -0500

    fix: :bug: remove auth0

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import {Auth0Provider} from '@auth0/auth0-react'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <Auth0Provider 
-  domain='dev-8zg23n3hta4o48tj.us.auth0.com'
-  clientId='L5CWawwQ8RkXBqw0rLZek7eeXvcJAYo5'
-  authorizationParams={{
-    redirect_uri: window.location.origin
-  }}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  </Auth0Provider>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
